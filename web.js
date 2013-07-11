645871f6a17798e12633aa00f23abbde7e88464e
Angel S <angelsalamanca@gmail.com>
2013-07-11 18:41:38 +0000

    updated web.js to read from index

--- web.js
+++ web.js
@@ -3,15 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-
-var buffer = fs.readFile('index.html', function (err, data) {
-    if (err) throw err;
-  console.log(data);
-});
+var buf = fs.readFileSync('index.html'); 
 
 App.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8'));
+    response.send(buf.toString("utf-8"));
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
